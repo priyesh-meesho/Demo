533f7537591737dce13ca8f740dcb3d36b1b289b
Priyesh Raj <priyeshraj@Meeshos-MacBook-Pro.local>
2022-01-11 15:57:27 +0530

    saving rows to database

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.util.Calendar.MAY;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+                Student priyesh = new Student(
+                        "Priyesh",
+                        "iec2018048@iiita.ac.in",
+                        21,
+                        LocalDate.of(2000, MAY,28)
+                );
+                Student raj = new Student(
+                        "Raj",
+                        "iec2018050@iiita.ac.in",
+                        21,
+                        LocalDate.of(2001, MAY,28)
+                );
+
+                repository.saveAll(List.of(priyesh,raj));
+        };
+    }
+}
